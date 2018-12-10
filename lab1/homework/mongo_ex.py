4a15e7d93932df0e3d0ce8ac8273d05ce9ab8444
Kedgar Nguyen <kedgarnguyen298@gmail.com>
2018-12-10 22:59:48 +0700

    commit initial

--- lab1/homework/mongo_ex.py
+++ lab1/homework/mongo_ex.py
@@ -9,9 +9,9 @@ db = client.get_database()
 post_collection = db["post"]
 
 status = {
-    "name": "Nguyễn Quốc Khánh",
-    "about c4e24": "Vài dòng về lớp hai tư. Thầy Huy, anh Đức rất ư nhiệt tình. Slide cả các giáo trình. Chi tiết, dễ hiểu về nhà học ngon. Kiến thức em vẫn còn non. Mong thầy chỉ bảo sau còn kiếm cơm ! ",
-    "say to next general": "Ba bốn triệu có là bao. Dăm ba cái thẻ cào game chứ gì. Học thấy hiệu quả tức thì. Nghỉ game đến với thầy Huy ngay nào :D "
+    "title": "About c4e24",
+    "author": "Nguyễn Quốc Khánh",
+    "content": "Vài dòng về lớp hai tư. Thầy Huy, anh Đức rất ư nhiệt tình. Slide cả các giáo trình. Chi tiết, dễ hiểu về nhà học ngon. Kiến thức em vẫn còn non. Mong thầy chỉ bảo sau còn kiếm cơm ! .Ba bốn triệu có là bao. Dăm ba cái thẻ cào game chứ gì. Học thấy hiệu quả tức thì. Nghỉ game đến với thầy Huy ngay nào :D "
 }
 
 post_collection.insert_one(status)
